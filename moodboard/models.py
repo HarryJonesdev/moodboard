4a5863f3e6c8d9ec7fe684a25da9060797190d77
Harry Jones <harryjones789@protonmail.com>
2023-03-29 17:44:12 +0100

    Added basic post functionality
    Admins can create a post utilising the built in admin dashboard
    Created post model
    Posts appear on the main page
    Added base html and home and about html
    Started styling

--- moodboard/models.py
+++ moodboard/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 
 class Post(models.Model):
@@ -10,7 +12,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-
+ 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
--- moodboard/views.py
+++ moodboard/views.py
@@ -1,3 +1,153 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+
+# Handle home page
+
+
+post = [
+    {
+        'author': 'Harry',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'John',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'Harry',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'John',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'Harry',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'John',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'Harry',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'John',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'Harry',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'John',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'Harry',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'John',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'Harry',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'John',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'Harry',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'John',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'Harry',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'John',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'Harry',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'John',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'Harry',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    },
+    {
+        'author': 'John',
+        'title': 'Title',
+        'content': ' Post content',
+        'date_posted': '27/03/23'
+    }
+
+]
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+        
+    }
+    return render(request, 'moodboard/home.html',context)
+
+def about(request):
+    return render(request, 'moodboard/about/')
+    
