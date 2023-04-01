e0976b8f057b95f440e7b7bba13e0d68465db324
Harry Jones <harryjones789@protonmail.com>
2023-04-01 16:22:47 +0100

    Added creating new users and login/logout functionality

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'moodboard',
+    'users',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -118,7 +121,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'moodboard-home'
+LOGIN_URL = 'login'
