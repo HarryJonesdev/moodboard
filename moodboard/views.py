e5d3b7d9836ec4ae0765d540eae6eaf2eed5e4de
Harry Jones <harryjones789@protonmail.com>
2023-04-06 15:22:05 +0100

    Added user posting, editing, deleting

--- moodboard/views.py
+++ moodboard/views.py
@@ -1,153 +1,85 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
-
-# Handle home page
-
-
-post = [
-    {
-        'author': 'Harry',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'John',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'Harry',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'John',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'Harry',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'John',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'Harry',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'John',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'Harry',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'John',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'Harry',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'John',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'Harry',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'John',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'Harry',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'John',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'Harry',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'John',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'Harry',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'John',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'Harry',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    },
-    {
-        'author': 'John',
-        'title': 'Title',
-        'content': ' Post content',
-        'date_posted': '27/03/23'
-    }
-
-]
 def home(request):
-    context = {
+    posts = {
         'posts': Post.objects.all()
-        
     }
-    return render(request, 'moodboard/home.html',context)
+    return render(request, 'moodboard/home.html',posts)
+
+class PostView(ListView):
+    model = Post
+
+    # <app>/<model>_<viewtype>.html
+    template_name = 'moodboard/home.html'
+    context_object_name = 'posts'
+
+    # Change order
+    ordering = ['-published_date']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    model = Post
+
+    # Establish form fields
+    fields = ['title', 'text']
+
+
+    # Override form valid method
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class UpdatePostView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+
+    # Establish form fields
+    fields = ['title', 'text']
+
+
+    # Override form valid method
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+
+class  DeletePostView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
 
 def about(request):
     return render(request, 'moodboard/about/')
+
     
+# References 
+
+# https://ccbv.co.uk/
